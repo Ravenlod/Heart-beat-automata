1fd20ffb627f8f5d2b873fe896559fee1fa4141f
Алексей <lyosha2002stich@gmail.com>
2023-09-19 09:57:07 +0500

    Sync

---
+++ utils.py
@@ -0,0 +1,12 @@
+class Neuron:
+    def __init__(self, value, *neighbour):
+        self.value = value
+        self.neighbour = neighbour
+        self
+        id+=1
+    
+
+id = 0
+
+my_neuron = Neuron(2, 1,2,3)
+print(my_neuron)
